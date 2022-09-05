a2c9d908aca23ebc41096517df6ac16ed002bbec
José Ríos <josemartzrios14@gmail.com>
2022-09-04 22:54:36 -0500

    about and navbar comps added

---
+++ src/components/about/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./About.css";
+
+
+const About = () => {
+  return (
+    <div className='about-container'>
+        <div className='about-desc'>
+        <h3>About me</h3>
+        <p>lorem ipsum</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
