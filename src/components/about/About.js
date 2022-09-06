aca3443e8a71dcbbe6684992469bfe9c1e832b52
José Ríos <josemartzrios14@gmail.com>
2022-09-05 19:28:57 -0500

    carousel

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,9 +6,13 @@ const About = () => {
   return (
     <div className='about-container'>
         <div className='about-desc'>
-        <h3>About me</h3>
-        <p>lorem ipsum</p>
+          <h3>About me</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
+
+        {/* <div className='about-img'>
+          <img src='https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt=''></img> 
+        </div> */}
     </div>
   )
 }
---
+++ src/components/slider/Slider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+
+
+const Slider = () => {
+  return (
+    <div className='carousel-container'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Slider
