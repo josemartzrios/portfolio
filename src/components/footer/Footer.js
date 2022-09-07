02fdc27ef451794c92b386d7210a4239be5a2cf4
José Ríos <josemartzrios14@gmail.com>
2022-09-07 18:31:33 -0500

    some changes 80%

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='footer-info'>
+            <h1>José Martínez Ríos</h1>
+            <p>Based in Mexico</p>
+        </div>
+        <div className='footer-contact'>
+            <h3>Contact me</h3>
+            <p>Let´s get down to work</p>
+        </div>
+        <div className='footer-sns'>
+            <div className='design-by'>
+                Created with React
+            </div>
+            <div className='sns-links'>
+                <a href='https://www.linkedin.com/in/jose-martinez-rios-frontend-developer/' target="_blank" rel="noreferrer">
+                    <i className='fab fa-linkedin linkedin'></i>
+                </a>
+                <a href='https://github.com/josemartzrios' target="_blank" rel="noreferrer">
+                    <i className="fa-brands fa-github github"></i>
+                </a>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Footer
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Carousel from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
+import Slides from './Slides';
+import "./Slider.css";
 
 
 const Slider = () => {
   return (
     <div className='carousel-container'>
-        <div></div>
+        <div className='carousel-title'>
+          <h3>My projects</h3>
+        </div>
+
+        <Carousel 
+          arrows
+          slidesPerPage={1}
+          infinite
+          animationSpeed={200}
+          centered
+          offset={50}
+          itemWidth={300}
+          slides={Slides}
+        />
     </div>
   )
 }
